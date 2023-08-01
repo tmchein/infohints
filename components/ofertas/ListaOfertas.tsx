dc3f827a0ee95cbbf755f0702a27bb03958c0fa5
Juan rojas <davidr2998@gmail.com>
2023-07-31 22:41:46 -0500

    test

--- components/ofertas/ListaOfertas.tsx
+++ components/ofertas/ListaOfertas.tsx
@@ -1,30 +1,30 @@
-"use client";
+// "use client";
 
-import Link from "next/link";
-import Oferta from "./Oferta";
-import { Oferta as OfertaType } from "./Ofertas.d";
+// import Link from "next/link";
+// import Oferta from "./Oferta";
+// import { Oferta as OfertaType } from "./Ofertas.d";
 
-interface ListaOfertasProps {
-  ofertas: OfertaType[];
-}
+// interface ListaOfertasProps {
+//   ofertas: OfertaType[];
+// }
 
-const ListaOfertas = ({ ofertas }: ListaOfertasProps) => {
-  return (
-    <ul className="grid grid-cols-3 gap-4">
-      {ofertas.map(({ id, title, salaryDescription, author }) => {
-        return (
-          <Link href={`/ofertas/${id}`}>
-            <Oferta
-              key={id}
-              title={title}
-              salaryDescription={salaryDescription}
-              authorName={author.name}
-            />
-          </Link>
-        );
-      })}
-    </ul>
-  );
-};
+// const ListaOfertas = ({ ofertas }: ListaOfertasProps) => {
+//   return (
+//     <ul className="grid grid-cols-3 gap-4">
+//       {ofertas.map(({ id, title, salaryDescription, author }) => {
+//         return (
+//           <Link href={`/ofertas/${id}`}>
+//             <Oferta
+//               key={id}
+//               title={title}
+//               salaryDescription={salaryDescription}
+//               authorName={author.name}
+//             />
+//           </Link>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
 
-export default ListaOfertas;
+// export default ListaOfertas;
